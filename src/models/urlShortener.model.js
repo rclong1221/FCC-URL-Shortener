e8c30094d6c9170c4bcd7fccf579c774cdd62f75
R.C. Long <deac.long@wsu.edu>
2018-03-27 04:38:09 -0700

    Change variable type of dependencies to constant

--- src/models/urlShortener.model.js
+++ src/models/urlShortener.model.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-var db = require("../../lib/db.js")
+const db = require("../../lib/db.js")
 
 var urlSchema = new Schema({
   originalUrl: String,
